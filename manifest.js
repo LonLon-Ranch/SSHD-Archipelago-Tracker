457ac0012d24e2ca0648e79b8a16c7d5fb85f048
robojumper <robojumper@gmail.com>
2025-01-15 22:09:03 +0100

    More PWA manifest

--- manifest.js
+++ manifest.js
@@ -5,18 +5,42 @@ export default function (startUrl = '/') {
         icons: [
             {
                 src: 'favicon.ico',
-                sizes: '64x64 32x32 24x24 16x16',
+                sizes: '64x64',
                 type: 'image/x-icon',
+                purpose: 'any',
             },
             {
                 src: 'logo192.png',
                 type: 'image/png',
                 sizes: '192x192',
+                purpose: 'any',
+            },
+            {
+                src: 'logo192-maskable.png',
+                type: 'image/png',
+                sizes: '192x192',
+                purpose: 'maskable',
             },
             {
                 src: 'logo512.png',
                 type: 'image/png',
                 sizes: '512x512',
+                purpose: 'any',
+            },
+        ],
+        screenshots: [
+            {
+                src: 'preview-wide.png',
+                sizes: '619x350',
+                type: 'image/png',
+                form_factor: 'wide',
+                label: 'Map Tracker',
+            },
+            {
+                src: 'preview-portrait.png',
+                sizes: '429x777',
+                type: 'image/png',
+                label: 'Item Tracker',
             },
         ],
         start_url: startUrl,
