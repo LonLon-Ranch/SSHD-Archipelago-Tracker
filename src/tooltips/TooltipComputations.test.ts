a36f81ee5aa41114bb5001620204ef4ad1ced563
robojumper <robojumper@gmail.com>
2025-01-31 22:16:43 +0100

    More tooltip tests, run tooltip tests in parallel

--- src/tooltips/TooltipComputations.test.ts
+++ src/tooltips/TooltipComputations.test.ts
@@ -2,6 +2,10 @@ import {
     trickSemiLogicSelector,
     trickSemiLogicTrickListSelector,
 } from '../customization/Selectors';
+import {
+    setEnabledSemilogicTricks,
+    setTrickSemiLogic,
+} from '../customization/Slice';
 import { mergeRequirements } from '../logic/bitlogic/BitLogic';
 import type BooleanExpression from '../logic/booleanlogic/BooleanExpression';
 import { logicSelector, optionsSelector } from '../logic/Selectors';
@@ -60,23 +64,32 @@ describe('tooltips', () => {
         expr = computer.getSnapshot(checkId);
         if (!expr) {
             let doResolve: (() => void) | undefined;
-            const resultPromise = new Promise<void>((resolve) => {
+            const callback = () => {
+                doResolve!();
+            };
+            let resultPromise = new Promise<void>((resolve) => {
                 doResolve = resolve;
             });
-            // Results aren't available, so we need to await.
-            // We only await once here, and thus verify there are no spurious wakeups.
-            // React's useSyncExternalStore can deal with spurious wakeups but this
-            // seems fine and I don't want to deal with more promises.
-            const unsubscribe = computer.subscribe(checkId, doResolve!);
-            await resultPromise;
+            const unsubscribe = computer.subscribe(checkId, callback);
             expr = computer.getSnapshot(checkId);
+            while (!expr) {
+                // Results aren't available, so we need to await.
+                await resultPromise;
+                expr = computer.getSnapshot(checkId);
+                if (expr) {
+                    break;
+                }
+                resultPromise = new Promise<void>((resolve) => {
+                    doResolve = resolve;
+                });
+            }
             unsubscribe();
             expect(expr).toBeDefined();
         }
 
         return booleanExprToTooltipExpr(
             tester.readSelector(logicSelector),
-            expr!,
+            expr,
             tester.readSelector(getRequirementLogicalStateSelector),
         );
     }
@@ -89,7 +102,7 @@ describe('tooltips', () => {
         }
     }
 
-    describe('all checks enabled', () => {
+    describe('basic checks', () => {
         let computer: TooltipComputer;
         beforeAll(() => {
             tester.reset();
@@ -103,7 +116,7 @@ describe('tooltips', () => {
             computer = createComputer();
         });
 
-        it('computes Nothing', async () => {
+        it.concurrent('computes Nothing', async ({ expect }) => {
             const result = await getTooltipExpression(
                 computer,
                 tester.findCheckId('Upper Skyloft', "Fledge's Gift"),
@@ -111,7 +124,7 @@ describe('tooltips', () => {
             expect(formatExpr(result)).toMatchInlineSnapshot(`"(Nothing)"`);
         });
 
-        it('computes Batreaux', async () => {
+        it.concurrent('computes Batreaux', async ({ expect }) => {
             const result = await getTooltipExpression(
                 computer,
                 tester.findCheckId("Batreaux's House", '30 Crystals Chest'),
@@ -121,17 +134,20 @@ describe('tooltips', () => {
             );
         });
 
-        it('computes more complicated things', async () => {
-            const result = await getTooltipExpression(
-                computer,
-                tester.findCheckId('Lanayru Desert', 'Rescue Caged Robot'),
-            );
-            expect(formatExpr(result)).toMatchInlineSnapshot(
-                `"(Amber Tablet and (Bomb Bag or (Hook Beetle and (Bow or Practice Sword))))"`,
-            );
-        });
+        it.concurrent(
+            'computes more complicated things',
+            async ({ expect }) => {
+                const result = await getTooltipExpression(
+                    computer,
+                    tester.findCheckId('Lanayru Desert', 'Rescue Caged Robot'),
+                );
+                expect(formatExpr(result)).toMatchInlineSnapshot(
+                    `"(Amber Tablet and (Bomb Bag or (Hook Beetle and (Bow or Practice Sword))))"`,
+                );
+            },
+        );
 
-        it('computes goddess chest', async () => {
+        it.concurrent('computes goddess chest', async ({ expect }) => {
             const result = await getTooltipExpression(
                 computer,
                 tester.findCheckId(
@@ -144,13 +160,53 @@ describe('tooltips', () => {
             );
         });
 
-        it('correctly factors out combinations', async () => {
+        it.concurrent(
+            'correctly factors out combinations',
+            async ({ expect }) => {
+                const result = await getTooltipExpression(
+                    computer,
+                    tester.findCheckId('Faron Woods', 'Deep Woods Chest'),
+                );
+                expect(formatExpr(result)).toMatchInlineSnapshot(
+                    `"(Emerald Tablet and (Clawshots or ((Bomb Bag or Practice Sword) and (Beetle or Bow or Slingshot))))"`,
+                );
+            },
+        );
+
+        it.concurrent('shows no tricks by default', async ({ expect }) => {
             const result = await getTooltipExpression(
                 computer,
-                tester.findCheckId('Faron Woods', 'Deep Woods Chest'),
+                tester.findCheckId('Eldin Volcano', 'Digging Spot below Tower'),
             );
             expect(formatExpr(result)).toMatchInlineSnapshot(
-                `"(Emerald Tablet and (Clawshots or ((Bomb Bag or Practice Sword) and (Beetle or Bow or Slingshot))))"`,
+                `"(Digging Mitts and Ruby Tablet and (Bow or Slingshot))"`,
+            );
+        });
+    });
+
+    describe('trick logic', () => {
+        let computer: TooltipComputer;
+        beforeAll(() => {
+            tester.reset();
+
+            const settings = tester.readSelector(allSettingsSelector);
+            tester.dispatch(
+                acceptSettings({
+                    settings: { ...settings, 'excluded-locations': [] },
+                }),
+            );
+            tester.dispatch(setTrickSemiLogic(true));
+            tester.dispatch(setEnabledSemilogicTricks(['Stuttersprint']));
+            computer = createComputer();
+        });
+
+        it.concurrent('shows tricks if asked', async ({ expect }) => {
+            const result = await getTooltipExpression(
+                computer,
+                tester.findCheckId('Eldin Volcano', 'Digging Spot below Tower'),
+            );
+            expect(formatExpr(result)).toMatchInlineSnapshot(
+                `"(Digging Mitts and Ruby Tablet and (Bow or Slingshot or Stuttersprint Trick))"`,
             );
         });
     });
